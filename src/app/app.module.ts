cb7169212428ab5d2b3696f578935c202ffe5ae9
Keegan Spell <keeganspell27@gmail.com>
2022-03-31 07:55:42 -0500

    the real project begins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SalesComponent } from './sales/sales.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SalesComponent,
+    InventoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
