2d816c4fbb25d07ff42e045d80d4d9a5e848dbfb
Keegan Spell <keeganspell27@gmail.com>
2022-04-11 13:48:24 -0500

    sorted imports in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+// components
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SalesComponent } from './sales/sales.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AuthGuardService } from "./services/auth-guard.service";
+
+//modules
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from "@angular/material/icon";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatCardModule } from "@angular/material/card";
-import { MatTableModule } from "@angular/material/table";
-import { MatDialogModule } from "@angular/material/dialog";
+import { BrowserModule } from '@angular/platform-browser';
 import { DialogModule } from "./dialogs/dialog.module";
-import { DialogService } from "./dialogs/dialog.service";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatTableModule } from "@angular/material/table";
+
+// services
+import { AuthGuardService } from "./services/auth-guard.service";
+import { DialogService } from "./dialogs/dialog.service";
+import { SnackbarService } from "./services/snackbar.service";
+
+// misc
+import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { Interceptor } from "./models/interceptor";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { SnackbarService } from "./services/snackbar.service";
 
 @NgModule({
   declarations: [
@@ -36,24 +41,23 @@ import { SnackbarService } from "./services/snackbar.service";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatTableModule,
-    MatDialogModule,
     DialogModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
     MatSnackBarModule,
+    MatTableModule,
+    ReactiveFormsModule,
   ],
   providers: [
-    AuthGuardService,
     AppComponent,
-    NavbarComponent,
+    AuthGuardService,
     DialogService,
+    NavbarComponent,
     SnackbarService,
     { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true }
   ],
