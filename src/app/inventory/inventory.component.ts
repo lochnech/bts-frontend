656747f96111aff46cafa6323dba684a7a31fd22
Tashon <washingt05@student.psdr3.org>
2022-04-25 08:18:17 -0500

    added inventory

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,4 +1,22 @@
+
 import { Component, OnInit } from '@angular/core';
+import {InventoryService} from "../services/inventory.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {DialogService} from "../dialogs/dialog.service";
+import {SnackbarService} from "../services/snackbar.service";
+
+
+export interface StoreItem {
+
+  barcode: string;
+  name: string;
+  price: number;
+  stock: number;
+
+
+}
+
 
 @Component({
   selector: 'app-inventory',
@@ -6,8 +24,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent implements OnInit {
+  data: StoreItem [];
+  totalPrice: number;
+  barcodeForm: FormGroup;
 
-  constructor() { }
+
+  displayedColumns = ['barcode','name','price','stock'];
+  dataSource: MatTableDataSource<StoreItem>;
+
+  constructor( public formBuilder: FormBuilder, public inventoryService: InventoryService,public dialogService: DialogService, public snackbar: SnackbarService  ) {
+    this.data = [];
+    this.totalPrice = 0;
+    this.dataSource = new MatTableDataSource<StoreItem>()
+
+    this.inventoryService.getInventory().subscribe(answers => {
+      this.dataSource.data = answers;
+    });
+    this.barcodeForm = new FormGroup({
+      barcodeText: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[0-9]+")
+      ])
+    });
+
+    this.updateData()
+
+
+  }
+
+
+  updateData(): void {
+    this.dataSource.data = this.data;
+  }
+
 
   ngOnInit(): void {
   }
