4c59a148ec546a2b59994e0057d0cef256960fc4
bowersj00 <bowersj00@student.psdr3.org>
2022-04-26 13:16:47 -0500

    fixed add item button

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,74 +1,60 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {InventoryService} from "../services/inventory.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {DialogService} from "../dialogs/dialog.service";
 import {SnackbarService} from "../services/snackbar.service";
 
-
 export interface StoreItem {
-
   barcode: string;
   name: string;
   price: number;
   stock: number;
-
-
 }
-
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
-export class InventoryComponent implements OnInit {
+
+export class InventoryComponent {
   data: StoreItem [];
   totalPrice: number;
   barcodeForm: FormGroup;
 
-
   displayedColumns = ['barcode','name','price','stock','edit'];
   dataSource: MatTableDataSource<StoreItem>;
 
-  constructor( public formBuilder: FormBuilder, public inventoryService: InventoryService,public dialogService: DialogService, public snackbar: SnackbarService  ) {
+  constructor(public inventoryService: InventoryService,public dialogService: DialogService, public snackbar: SnackbarService) {
     this.data = [];
     this.totalPrice = 0;
     this.dataSource = new MatTableDataSource<StoreItem>()
 
-    this.inventoryService.getInventory().subscribe(answers => {
-      this.dataSource.data = answers;
-    });
     this.barcodeForm = new FormGroup({
       barcodeText: new FormControl('', [
         Validators.required,
         Validators.pattern("[0-9]+")
       ])
     });
-
     this.updateData()
-
-
   }
 
   updateData(): void {
-    this.dataSource.data = this.data;
+    this.inventoryService.getInventory().subscribe(answers => {
+      this.dataSource.data = answers;
+      this.data = answers
+    });
   }
 
   addItem() {
     this.dialogService.openAddItem().then(ans => {
-      this.inventoryService.addItem(ans).subscribe()
-      this.updateData();
+      this.inventoryService.addItem(ans).subscribe(ans => this.updateData());
     });
-
   }
 
   editItem(item: StoreItem) {
     console.log('editing item: ' + item);
-  }
-
-  ngOnInit(): void {
   }
 
 }
