92e4fc1790749a1833bc420e6a87de4a86c15b8b
Keegan Spell <keeganspell27@gmail.com>
2022-04-28 21:33:28 -0500

    refactored dialogs into separate folders

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -3,12 +3,12 @@ import { NgModule} from '@angular/core';
 import { DialogService } from './dialog.service';
 import { MatDialogModule} from '@angular/material/dialog';
 import { FormsModule} from "@angular/forms";
-import { ConfirmDialogComponent } from './confirm-dialog.component';
-import { AddItemDialogComponent } from './add-item-dialog.component';
-import { EditItemDialogComponent } from "./edit-item-dialog.component";
-import { ConfirmDeleteDialogComponent } from "./confirm-delete-dialog.component";
-import { MakeChangeDialogComponent } from "./make-change-dialog.component";
-import { ViewItemsDialogComponent } from "./view-items-dialog.component";
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { AddItemDialogComponent } from './add-item-dialog/add-item-dialog.component';
+import { EditItemDialogComponent } from "./edit-item-dialog/edit-item-dialog.component";
+import { ConfirmDeleteDialogComponent } from "./confirm-delete-dialog/confirm-delete-dialog.component";
+import { MakeChangeDialogComponent } from "./make-change-dialog/make-change-dialog.component";
+import { ViewItemsDialogComponent } from "./view-items-dialog/view-items-dialog.component";
 
 @NgModule({
   imports: [
