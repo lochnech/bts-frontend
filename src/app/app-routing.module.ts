176ba74623d9ca380374b3e1466658e71ae8e222
Tashon <washingt05@student.psdr3.org>
2022-05-09 12:19:54 -0500

    added view users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { WelcomeComponent } from "./welcome/welcome.component";
 import { SalesComponent } from "./sales/sales.component";
 import { InventoryComponent } from "./inventory/inventory.component";
 import { AuthGuardService } from "./services/auth-guard.service";
+import {ViewUsersComponent} from "./view-users/view-users.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/welcome', pathMatch: 'full'},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'sales', component: SalesComponent, canActivate: [AuthGuardService]},
   {path: 'inventory', component: InventoryComponent, canActivate: [AuthGuardService]},
+  {path: 'view_users', component:ViewUsersComponent, canActivate: [AuthGuardService]},
   {path: '**', redirectTo: '/welcome'}
 ];
 
--- src/app/view-users/view-users.component.ts
+++ src/app/view-users/view-users.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../services/user.service";
+import {MatTableDataSource} from "@angular/material/table";
+
+
+
+export interface User {
+  username: string;
+  password: string;
+}
+
+
 
 @Component({
   selector: 'app-view-users',
+  styleUrls: ['./view-users.component.css'],
   templateUrl: './view-users.component.html',
-  styleUrls: ['./view-users.component.css']
 })
 export class ViewUsersComponent implements OnInit {
+  displayedColumns: string[] = ['username', 'password'];
+  dataSource: MatTableDataSource<any>
 
-  constructor() { }
+
+
+
+  constructor(private userService: UserService) {
+    this.dataSource = new MatTableDataSource<any>()
+
+  }
+
+
+
+
 
   ngOnInit(): void {
   }
