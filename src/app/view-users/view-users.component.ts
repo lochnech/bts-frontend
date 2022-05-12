ed8c08d5111f29ac44c9bf69a3703262c568efec
Tashon <washingt05@student.psdr3.org>
2022-05-12 07:46:36 -0500

    updated view users

--- src/app/view-users/view-users.component.ts
+++ src/app/view-users/view-users.component.ts
@@ -18,17 +18,49 @@ export interface User {
   templateUrl: './view-users.component.html',
 })
 export class ViewUsersComponent implements OnInit {
-  displayedColumns: string[] = ['username', 'password', 'boolean'];
-  dataSource: MatTableDataSource<any>
+  users: User[];
+
+  displayedColumns = ['username', 'password', 'boolean'];
+  dataSource: MatTableDataSource<User>
+
 
 
 
 
   constructor(private userService: UserService) {
-    this.dataSource = new MatTableDataSource<any>()
+    this.users = [];
+    this.dataSource = new MatTableDataSource<User>()
 
 
+  }
 
+  updateUsers(): void {
+    this.userService.getUsers().subscribe(answers => {
+      this.dataSource.data = answers;
+      this.users = answers
+    });
+  }
+
+  addUser(){
+    this.dialogService.openAddUser().then(ans => {
+      this.updateUsers();
+
+    }).catch(ans => this.snackbar.open("User not added" , "Dismiss", 5000));
+
+  }
+
+  editUser(user: User){
+    this.dialogService.openEditUser(user).then(ans =>{
+      this.userService.changeUser(User.username, ans).subscribe(ans => this.updateUsers());
+    }).catch(ans => this.snackbar.open("Something went wrong editing user" , "Dismiss", 5000));
+  }
+
+  deleteUser(user:User) {
+    this.dialogService.openConfirmDelete(user).then(ans =>{
+      if(ans){
+        this.userService.deleteUser(user.username).subscribe(ans => this.updateUsers());
+      }
+    }).catch(ans => this.snackbar.open("Something went wrong deleting user" , "Dismiss", 5000));
   }
 
 
